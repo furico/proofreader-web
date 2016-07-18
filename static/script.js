75a379324a25a16707649c466f1f6126f25fc9a0
Hiroyuki Furihata <furihatah@gmail.com>
2016-07-18 22:52:39 +0900

    initial commit

---
+++ static/script.js
@@ -0,0 +1,31 @@
+// 編集ルールを取得する
+var get_rules = (function() {
+  var rules = null;
+  var request = window.superagent;
+  request.get("static/rules.json").end(function(err, res) {
+    rules = res.body;
+  });
+  return function() {
+    return rules;
+  }
+})();
+function filter_kanji(text) {
+  var rules = get_rules();
+  rules['kanji'].forEach(function(kanji, index) {
+    var re = new RegExp(kanji, 'g');
+    var tagged = '<span class="found">' + kanji + '</span>';
+    text = text.replace(re, tagged);
+  });
+  return text
+}
+Vue.config.delimiters = ['${', '}']
+Vue.filter('check', function (value) {
+  if (value)
+    return filter_kanji(value)
+})
+new Vue({
+  el: '#editor',
+  data: {
+    input: ''
+  }
+});
