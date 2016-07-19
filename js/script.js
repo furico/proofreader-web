848300769af8bda4b246a19b50bf6e019bcea6ea
Hiroyuki Furihata <furihatah@gmail.com>
2016-07-19 22:14:13 +0900

    グローバル名前空間の汚染防止

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-// 編集ルールを取得する
-var get_rules = (function() {
-  var rules = null;
-  var request = window.superagent;
-  request.get("/json/rules.json").end(function(err, res) {
-    rules = res.body;
-  });
-  return function() {
-    return rules;
+(function(global) {
+  'use strict';
+  // 編集ルールを取得する
+  var get_rules = (function() {
+    var rules = null;
+    var request = global.superagent;
+    request.get("/json/rules.json").end(function(err, res) {
+      rules = res.body;
+    });
+    return function() {
+      return rules;
+    }
+  })();
+  function filter_kanji(text) {
+    var rules = get_rules();
+    rules['kanji'].forEach(function(kanji, index) {
+      var re = new RegExp(kanji, 'g');
+      var tagged = '<span class="found">' + kanji + '</span>';
+      text = text.replace(re, tagged);
+    });
+    return text
   }
-})();
-function filter_kanji(text) {
-  var rules = get_rules();
-  rules['kanji'].forEach(function(kanji, index) {
-    var re = new RegExp(kanji, 'g');
-    var tagged = '<span class="found">' + kanji + '</span>';
-    text = text.replace(re, tagged);
+  Vue.filter('check', function (value) {
+    if (value)
+      return filter_kanji(value)
+  })
+  new Vue({
+    el: '#editor',
+    data: {
+      input: ''
+    }
   });
-  return text
-}
-Vue.config.delimiters = ['${', '}']
-Vue.filter('check', function (value) {
-  if (value)
-    return filter_kanji(value)
-})
-new Vue({
-  el: '#editor',
-  data: {
-    input: ''
-  }
-});
+})(this);
