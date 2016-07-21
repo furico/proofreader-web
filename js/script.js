d90d6d69cca9b433323ffe226140a0e6c4dc8d03
Hiroyuki Furihata <furihatah@gmail.com>
2016-07-21 21:08:39 +0900

    インフォメッセージを表示するようにした

--- js/script.js
+++ js/script.js
@@ -13,34 +13,68 @@
   })();
   function filter_kanji(text) {
     var rules = get_rules();
-    rules['kanji'].forEach(function(kanji, index) {
-      var re = new RegExp(kanji, 'g');
-      var tagged = '<span class="found">' + kanji + '</span>';
-      text = text.replace(re, tagged);
+    var targets = rules['kanji']['targets'];
+    targets.forEach(function(target, index) {
+      var re = new RegExp(target['data'], 'g');
+      var span = global.document.createElement('span');
+      span.className = 'found';
+      span.dataset.rule = 'kanji';
+      span.dataset.target = target['data'];
+      span.textContent = target['data'];
+      text = text.replace(re, span.outerHTML);
     });
     return text
   }
   function filter_saidoku(text) {
     var rules = get_rules();
-    rules['saidoku'].forEach(function(saidoku, index) {
-      var re = new RegExp(saidoku, 'g');
-      var tagged = '<span class="found">' + saidoku + '</span>';
+    var targets = rules['saidoku']['targets'];
+    targets.forEach(function(target, index) {
+      var re = new RegExp(target['data'], 'g');
+      var tagged = '<span class="found" data-hoge="bbb">' + target['data'] + '</span>';
       text = text.replace(re, tagged);
     });
     return text
   }
-  Vue.filter('check', function (value) {
-    var result = '';
-    if (value) {
-      result = filter_kanji(value);
-      result = filter_saidoku(result);
-      return result;
-    }
-  })
   new Vue({
-    el: '#editor',
+    el: '#app',
     data: {
-      input: ''
+      input: '',
+      info: {name: '', desc: '', target: '', note: ''}
+    },
+    methods: {
+      showMessage: function(event) {
+        var tg = event.target;
+        if (tg.className !== 'found') {
+          this.info.name = '';
+          this.info.desc = '';
+          this.info.target = '';
+          this.info.note = '';
+          return;
+        }
+        var rules = get_rules();
+        var rule = rules[tg.dataset.rule];
+        var target = null;
+        rule.targets.forEach(function(elm, index) {
+          if (elm.data === tg.dataset.target) {
+            target = elm;
+            return;
+          }
+        })
+        this.info.name = rule.name;
+        this.info.desc = rule.desc;
+        this.info.target = target.data;
+        this.info.note = target.note;
+      }
+    },
+    filters: {
+      check: function (value) {
+        var result = '';
+        if (!value)
+          return;
+        result = filter_kanji(value);
+        result = filter_saidoku(result);
+        return result;
+      }
     }
   });
 })(this);
