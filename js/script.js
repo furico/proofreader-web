d0d30658d71b4fcc6be3aba7e4389dc99dad3378
Hiroyuki Furihata <furihatah@gmail.com>
2016-07-25 22:00:43 +0900

    リソースファイルへのパスを相対パスにした

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
   var get_rules = (function() {
     var rules = null;
     var request = global.superagent;
-    request.get("/json/rules.json").end(function(err, res) {
+    request.get("json/rules.json").end(function(err, res) {
       rules = res.body;
     });
     return function() {
